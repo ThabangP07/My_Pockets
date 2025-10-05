31b372237b4ce94cc6d6a3675f286889da281f12
Thabang <phoshokothabang1@gmail.com>
2025-10-05 20:26:57 +0200

    feat: create an exchange base component

---
+++ My_Pockets/src/components/Exchange.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+// i will pull the data from the forex exchange api/database in real time
+function Exchange() {
+  return (
+    <div>
+      <h2>Forex Exchange</h2>
+      <div>
+        <div>
+          <p>USD/ZAR</p>
+          <p>Rates</p>
+        </div>
+        <div>
+          <p>USD/ZAR</p>
+          <p>Rates</p>
+        </div>
+        <div>
+          <p>USD/ZAR</p>
+          <p>Rates</p>
+        </div>
+        <div>
+          <p>USD/ZAR</p>
+          <p>Rates</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Exchange
