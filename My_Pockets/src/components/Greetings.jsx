b44c7c9159c5f735eaad0b965aec476fc0da7d6c
Thabang <phoshokothabang1@gmail.com>
2025-10-05 20:37:52 +0200

    feat: create a greeting template banner

---
+++ My_Pockets/src/components/Greetings.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Greetings() {
+  return (
+    <div>
+      <h1>Good Day, Thabang</h1>
+      <input type="text" placeholder='Search'/>
+      <p>Thabang P.</p> // profile placeholder
+    </div>
+  )
+}
+
+export default Greetings
