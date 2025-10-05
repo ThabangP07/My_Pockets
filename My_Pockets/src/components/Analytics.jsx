cd0b864b1b3fafc28914d41d5a01b948a9cdaa4e
Thabang <phoshokothabang1@gmail.com>
2025-10-05 19:21:25 +0200

    feat: create an Analytics base component

---
+++ My_Pockets/src/components/Analytics.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Analytics() {
+  return (
+    <div>
+      <h2>Analytics</h2>
+      <button>6 Months</button>
+      <button>Year</button>
+      <div>
+        <div>
+          // Chart Placeholder
+          <p>Month</p>
+        </div>
+        <div>
+          // Chart Placeholder
+          <p>Month</p>
+        </div>
+        <div>
+          // Chart Placeholder
+          <p>Month</p> // month Placeholder
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Analytics
