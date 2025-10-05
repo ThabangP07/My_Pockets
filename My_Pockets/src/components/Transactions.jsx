d36044577744b28ae3c20a4691a9493e7734e2bf
Thabang <phoshokothabang1@gmail.com>
2025-10-05 20:22:27 +0200

    feat: create a transactions base component

---
+++ My_Pockets/src/components/Transactions.jsx
@@ -0,0 +1,28 @@
+// transactions will be pulled out of json file
+
+import React from 'react'
+
+function Transactions() {
+  return (
+    <div>
+      <h2>Transactions</h2>
+      <p>Today</p>
+      <div>
+        <div>
+          <p>Amazon</p>
+          <p>R200.00</p>
+        </div>
+        <div>
+          <p>Amazon</p>
+          <p>R200.00</p>
+        </div>
+        <div>
+          <p>Amazon</p>
+          <p>R200.00</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Transactions
