42b9a87260f52be0f33972242c376fc184ba36c2
Thabang <phoshokothabang1@gmail.com>
2025-10-05 19:16:13 +0200

    feat: create a base overview component

---
+++ My_Pockets/src/components/Overview.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Overview() {
+  return (
+    <section>
+      <h2>Overview</h2>
+      <div>
+        <h3>Balance</h3>
+        <p>amount</p>
+      </div>
+      <div>
+        <h3>Income</h3>
+        <p>amount</p>
+      </div>
+      <div>
+        <h3>Expenses</h3>
+        <p>amount</p>
+      </div>
+    </section>
+  )
+}
+
+export default Overview
