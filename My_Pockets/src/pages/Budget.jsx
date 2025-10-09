e2179456ef051c373632af4019043f3f5ee52e15
Thabang <phoshokothabang1@gmail.com>
2025-10-09 19:19:01 +0200

    refactor: enhance the budget component to display the individual cards

--- My_Pockets/src/pages/Budget.jsx
+++ My_Pockets/src/pages/Budget.jsx
@@ -1,42 +1,12 @@
 import React from 'react'
+import BudgetCard from './BudgetCard';
 
 function Budget() {
   return (
     <div>
       <h1>Budget</h1>
       <div>
-        <div>
-          <h4>Expected Income</h4>
-          <div>
-            <p>Company Inc</p>
-            <p>amount</p>
-          </div>
-          <p>Total: #######</p>
-        </div>
-        <div>
-          <h4>Monthly Subscriptions</h4>
-          <div>
-            <p>Company Inc</p>
-            <p>amount</p>
-          </div>
-          <p>Total: #######</p>
-        </div>
-        <div>
-          <h4>Family Expenses</h4>
-          <div>
-            <p>Grocery</p>
-            <p>amount</p>
-          </div>
-          <p>Total: #######</p>
-        </div>
-        <div>
-          <h4>Loans and Insurances</h4>
-          <div>
-            <p>Loans</p>
-            <p>amount</p>
-          </div>
-          <p>Total: #######</p>
-        </div>
+        <BudgetCard heading={"Expected Income"} item={"Company A"} amount={"R 2000"} />
       </div>
 
       <h5>Summary:</h5>
