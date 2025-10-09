7cf5fc7293fea4ac8ec28d9eb79d92c5fce35557
Thabang <phoshokothabang1@gmail.com>
2025-10-09 18:27:19 +0200

    feat: Enhance the transaction component to use props for reusability

--- My_Pockets/src/components/Transactions.jsx
+++ My_Pockets/src/components/Transactions.jsx
@@ -2,23 +2,15 @@
 
 import React from 'react'
 
-function Transactions() {
+function Transactions( transaction) {
   return (
     <div>
       <h2>Transactions</h2>
       <p>Today</p>
       <div>
         <div>
-          <p>Amazon</p>
-          <p>R200.00</p>
-        </div>
-        <div>
-          <p>Amazon</p>
-          <p>R200.00</p>
-        </div>
-        <div>
-          <p>Amazon</p>
-          <p>R200.00</p>
+          <p>{transaction.store}</p>
+          <p>{transaction.price}</p>
         </div>
       </div>
     </div>
