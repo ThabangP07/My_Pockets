75b5e6844d5eda22c6db80f0d2d2712e37283576
Thabang <phoshokothabang1@gmail.com>
2025-10-09 19:36:25 +0200

    refactor: enhance the overview component to display the 3 simultaneously

--- My_Pockets/src/components/Overview.jsx
+++ My_Pockets/src/components/Overview.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
+import OverviewItem from './OverviewItem'
 
-function Overview( prop) {
+function Overview( prop ) {
   return (
     <section>
       <h2>Overview</h2>
-      <div>
-        <h3>{prop.heading}</h3>
-        <p>{prop.amount}</p>
-      </div>
+      <OverviewItem heading={prop.heading1} amount={prop.amount1} />
+      <OverviewItem heading={prop.heading2} amount={prop.amount2} />
+      <OverviewItem heading={prop.heading3} amount={prop.amount3} />
     </section>
   )
 }
