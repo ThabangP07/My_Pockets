a09c81b4c1e7210833a37fdec6c8e38ddad4980c
Thabang <phoshokothabang1@gmail.com>
2025-10-09 18:28:52 +0200

    feat: test the transaction component to display multiple transactions through a loop

--- My_Pockets/src/App.jsx
+++ My_Pockets/src/App.jsx
@@ -1,10 +1,21 @@
+import Transactions from "./components/Transactions"
 
 function App() {
+
+  // hardcoded transactions data, to be replaced with json data
+  const transaction = [{ store: "Amazon", price: "R200.00" },
+                        { store: "Walmart", price: "R150.00" },
+                        { store: "Ebay", price: "R300.00" },
+                        { store: "Target", price: "R400.00" },
+                        { store: "Best Buy", price: "R250.00" }
+  ];
 
   return (
     <>
       <h1>My Pockets</h1>
       <p>Personal Finance Tracker</p>
+      {transaction.map((trans) => 
+        <Transactions store={trans.store} price={trans.price} />)}
     </>
   )
 }
