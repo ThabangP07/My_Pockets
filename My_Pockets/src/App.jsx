e54c739e2354d40dd29e49d21989627b61985876
Thabang <phoshokothabang1@gmail.com>
2025-10-09 18:46:41 +0200

    chore: testing the exchange component reusability

--- My_Pockets/src/App.jsx
+++ My_Pockets/src/App.jsx
@@ -1,21 +1,23 @@
+import Exchange from "./components/Exchange";
 import Transactions from "./components/Transactions"
 
 function App() {
 
-  // hardcoded transactions data, to be replaced with json data
-  const transaction = [{ store: "Amazon", price: "R200.00" },
-                        { store: "Walmart", price: "R150.00" },
-                        { store: "Ebay", price: "R300.00" },
-                        { store: "Target", price: "R400.00" },
-                        { store: "Best Buy", price: "R250.00" }
+  // hardcoded the data, to be replaced with data from an api
+  const rates = [
+    { header: "USD/ZAR", exchange: "R 17.36" },
+    { header: "EUR/ZAR", exchange: "R 20.07" },
+    { header: "EUR/ZAR", exchange: "R 20.07" },
+    { header: "EUR/ZAR", exchange: "R 20.07" },
+
   ];
 
   return (
     <>
       <h1>My Pockets</h1>
       <p>Personal Finance Tracker</p>
-      {transaction.map((trans) => 
-        <Transactions store={trans.store} price={trans.price} />)}
+      {rates.map((rate) => 
+        <Exchange header={rate.header} exchange={rate.exchange} />)}
     </>
   )
 }
