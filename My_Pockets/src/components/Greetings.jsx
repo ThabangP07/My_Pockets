5e4fc26eafeb122c11fb18f4608fecb2c95f3165
Thabang <phoshokothabang1@gmail.com>
2025-10-12 22:04:49 +0200

    style: style Greetings component with grid layout and styling

--- My_Pockets/src/components/Greetings.jsx
+++ My_Pockets/src/components/Greetings.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Greetings() {
   return (
-    <div>
-      <h1>Good Day, Thabang</h1>
-      <input type="text" placeholder='Search'/>
-      <p>Thabang P.</p> // profile placeholder
+    <div className="grid grid-cols-4 mt-2 mr-2 mb-5 py-2" >
+      <h1 className="text-4xl  font-bold col-span-2">Good Day, Thabang</h1>
+      <input className='border rounded-2xl p-2' type="text" placeholder="Search" />
+      <p className='font-bold ml-20'>Thabang P.</p>
     </div>
-  )
+  );
 }
 
 export default Greetings
