2579dfab6d5a102b1ffefe1cf3699b9a32abc01a
Thabang <phoshokothabang1@gmail.com>
2025-10-17 15:21:03 +0200

    feat: toggle form visibility for adding transactions

--- My_Pockets/src/components/Transactions.jsx
+++ My_Pockets/src/components/Transactions.jsx
@@ -4,20 +4,27 @@ import React from 'react'
 import { useState } from 'react';
 import userData from '../data/transaction.json'
 import TransactionItems from './TransactionItems';
+import TransactionsInputForm from './TransactionsInputForm';
 
 
 function Transactions() {
 
   const [showForm, setShowForm] = useState(false);
 
+  const handleAddTransactionClick = () => {
+    setShowForm(!showForm);
+  }
+
   return (
     <section className="border-2 rounded-2xl p-2 space-y-3">
       <h2 className="font-bold text-2xl">Transactions</h2>
       <div className="flex justify-between">
         <p>Today</p>
-        <button className="border-1 rounded-2xl p-2">
-          Add Transaction
+        <button className="border-1 rounded-2xl p-2"
+          onClick={handleAddTransactionClick}>
+          {showForm ? 'Close' : 'Add Transaction'}
         </button>
+      { showForm && <TransactionsInputForm /> }
       </div>
 
       <div>
