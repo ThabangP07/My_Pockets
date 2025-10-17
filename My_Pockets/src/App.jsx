ebe5abb510ed09ddce62dbd41b8fb1836fa79476
Thabang <phoshokothabang1@gmail.com>
2025-10-17 22:01:50 +0200

    refactor: reorganize App component structure for improved functionality and maintainability

--- My_Pockets/src/App.jsx
+++ My_Pockets/src/App.jsx
@@ -10,33 +10,43 @@ import Budget from "./pages/Budget";
 function App() {
 
   return (
-    <div className="grid grid-cols-4 h-dvh bg-blue-200">
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="grid grid-cols-4 h-dvh bg-blue-200">
         <NavBar />
-        <Routes>
-          <Route path="/budget" element={<Budget />} />
-        </Routes>
-      </BrowserRouter>
 
-      <main className="col-span-3">
-        <header>
-          <Greetings />
-        </header>
-        <section className="grid grid-cols-2 gap-2 mr-2">
-          <Overview
-            heading1="Balance"
-            heading2="Income"
-            heading3="Expenses"
-            amount1="R 2 000.00"
-            amount2="R 15 000.00"
-            amount3="R 7 000.00"
-          />
-          <Analytics />
-          <Transactions />
-          <Exchange />
-        </section>
-      </main>
-    </div>
+        <main className="col-span-3">
+          <Routes>
+            {/* Home route */}
+            <Route
+              path="/"
+              element={
+                <>
+                  <header>
+                    <Greetings />
+                  </header>
+                  <section className="grid grid-cols-2 gap-2 mr-2">
+                    <Overview
+                      heading1="Balance"
+                      heading2="Income"
+                      heading3="Expenses"
+                      amount1="R 2 000.00"
+                      amount2="R 15 000.00"
+                      amount3="R 7 000.00"
+                    />
+                    <Analytics />
+                    <Transactions />
+                    <Exchange />
+                  </section>
+                </>
+              }
+            />
+
+            {/* Budget route */}
+            <Route path="/budget" element={<Budget />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
