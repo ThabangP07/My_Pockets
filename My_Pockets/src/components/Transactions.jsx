a66931cf7f7132dda9686124d2278ba116315816
Thabang <phoshokothabang1@gmail.com>
2025-10-17 15:01:48 +0200

    feat: add an add transaction button for the form and style it

--- My_Pockets/src/components/Transactions.jsx
+++ My_Pockets/src/components/Transactions.jsx
@@ -1,17 +1,32 @@
 // transactions will be pulled out of json file
 
 import React from 'react'
+import { useState } from 'react';
 import userData from '../data/transaction.json'
 import TransactionItems from './TransactionItems';
 
+
 function Transactions() {
+
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <section className="border-2 rounded-2xl p-2 space-y-3">
       <h2 className="font-bold text-2xl">Transactions</h2>
-      <p>Today</p>
+      <div className="flex justify-between">
+        <p>Today</p>
+        <button className="border-1 rounded-2xl p-2">
+          Add Transaction
+        </button>
+      </div>
+
       <div>
         {userData.map((data) => (
-          <TransactionItems item={data.item} store={data.store} price={data.price} />
+          <TransactionItems
+            item={data.item}
+            store={data.store}
+            price={data.price}
+          />
         ))}
       </div>
     </section>
