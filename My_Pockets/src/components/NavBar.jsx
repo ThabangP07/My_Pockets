9153b885551c0a1e149488201828c28f7ad4cb7d
Thabang <phoshokothabang1@gmail.com>
2025-10-17 15:55:55 +0200

    refactor: update NavBar component to use Link for navigation

--- My_Pockets/src/components/NavBar.jsx
+++ My_Pockets/src/components/NavBar.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <nav className='bg-gray-600 border-2 rounded-2xl m-2'>
-      <h1 className='text-4xl m-2 py-7 text-orange-400'>MyPockets</h1>
-      <ul className='m-1 p-2 space-y-3 text-white'>
-        <li>home</li>
-        <li>Investments</li>
-        <li>Analytics</li>
-        <li>Budget</li>
-        <li>History</li>
-        <li>Tips</li>
+    <nav className="bg-gray-600 border-2 rounded-2xl m-2">
+      <h1 className="text-4xl m-2 py-7 text-orange-400">MyPockets</h1>
+      <ul className="m-1 p-2 space-y-3 text-white">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/investments">Investments</Link>
+        </li>
+        <li>
+          <Link to="/analytics">Analytics</Link>
+        </li>
+        <li>
+          <Link to="/budget">Budget</Link>
+        </li>
+        <li>
+          <Link to="/history">History</Link>
+        </li>
+        <li>
+          <Link to="/tips">Tips</Link>
+        </li>
       </ul>
-      <button className='text-amber-100 mt-100 px-2'>Settings</button>
-      
+      <button className="text-amber-100 mt-100 px-2">Settings</button>
     </nav>
-  )
+  );
 }
 
 export default NavBar
