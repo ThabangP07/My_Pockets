8c292ef07b187dfd9acbba20f2006c29db949054
Thabang <phoshokothabang1@gmail.com>
2025-10-19 08:20:17 +0200

    refactor: adjust the navbar for improved layout and display

--- My_Pockets/src/components/NavBar.jsx
+++ My_Pockets/src/components/NavBar.jsx
@@ -2,29 +2,31 @@ import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <nav className="bg-gray-600 border-2 rounded-2xl m-2">
-      <h1 className="text-4xl m-2 py-7 text-orange-400">MyPockets</h1>
-      <ul className="m-1 p-2 space-y-3 text-white">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/investments">Investments</Link>
-        </li>
-        <li>
-          <Link to="/analytics">Analytics</Link>
-        </li>
-        <li>
-          <Link to="/budget">Budget</Link>
-        </li>
-        <li>
-          <Link to="/history">History</Link>
-        </li>
-        <li>
-          <Link to="/tips">Tips</Link>
-        </li>
-      </ul>
-      <button className="text-amber-100 mt-100 px-2">Settings</button>
+    <nav className="flex flex-col justify-between bg-gray-600 border-2 rounded-2xl m-2">
+      <div>
+        <h1 className="text-4xl m-2 py-7 text-orange-400">MyPockets</h1>
+        <ul className="m-1 p-2 space-y-3 text-white">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/investments">Investments</Link>
+          </li>
+          <li>
+            <Link to="/analytics">Analytics</Link>
+          </li>
+          <li>
+            <Link to="/budget">Budget</Link>
+          </li>
+          <li>
+            <Link to="/history">History</Link>
+          </li>
+          <li>
+            <Link to="/tips">Tips</Link>
+          </li>
+        </ul>
+      </div>
+      <button className="text-amber-100 px-2 pb-3">Settings</button>
     </nav>
   );
 }
