c95600714c35d3fc8b1a49d7742eb81a723ce46e
Thabang <phoshokothabang1@gmail.com>
2025-10-19 14:13:57 +0200

    fix: update transaction prices to remove currency symbol and adjust loading logic for transactions

--- My_Pockets/src/components/TransactionItems.jsx
+++ My_Pockets/src/components/TransactionItems.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function TransactionItems({item, store, price}) {
   return (
-    <div className='flex justify-between space-y-2'>
+    <div className='grid grid-cols-3 space-y-2'>
       <p className='font-bold'>{item}</p>
       <p className='font-bold '>{store}</p>
-      <p>{price}</p>
+      <p>R{price}</p>
     </div>
   );
 }
