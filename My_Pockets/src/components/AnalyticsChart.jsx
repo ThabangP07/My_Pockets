a0f01c19c5995a63f60abf4f8c52524129a679ed
Thabang <phoshokothabang1@gmail.com>
2025-10-19 07:18:59 +0200

    feat: enhance AnalyticsChart to accept dynamic amount and maxAmount for proportional height calculation

--- My_Pockets/src/components/AnalyticsChart.jsx
+++ My_Pockets/src/components/AnalyticsChart.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function AnalyticsChart({month}) {
+function AnalyticsChart({ month, amount, maxAmount }) {
+  // calculate proportional height
+  const heightPercent = maxAmount > 0 ? (amount / maxAmount) * 100 : 0;
+  console.log({ month, amount, maxAmount, heightPercent });
   return (
-    <div>
-      <div className='bg-amber-500'></div>
-      <p>{month}</p>
+    <div className="flex flex-col items-center justify-end w-10">
+      <div
+        className="bg-amber-500 border rounded-t-lg transition-all duration-500"
+        style={{
+          height: `${heightPercent}%`,
+          width: "25px",
+          minHeight: "50px",
+        }}
+      ></div>
+      <p className="text-sm mt-1 font-medium">{month}</p>
     </div>
   );
 }
 
-export default AnalyticsChart
+export default AnalyticsChart;
