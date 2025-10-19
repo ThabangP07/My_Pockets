2e4f9fd62b2c67b06774b256a73cfe8b05a83599
Thabang <phoshokothabang1@gmail.com>
2025-10-19 08:11:14 +0200

    feat: add Footer component for website footer section

---
+++ My_Pockets/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="mt-4 p-4 text-center">
+      <p>Developed by <strong>Phoshoko Thabang</strong></p>
+      <p className='italic'>© 2025 My Pockets. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
