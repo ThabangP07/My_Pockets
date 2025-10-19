c841bf26d4dfc70a2e50c15f0b82ee666d1a771a
Thabang <phoshokothabang1@gmail.com>
2025-10-19 08:11:23 +0200

    refactor: improve layout structure in App component by removing unnecessary header and adjusting main section and adding the footer section

--- My_Pockets/src/App.jsx
+++ My_Pockets/src/App.jsx
@@ -5,53 +5,56 @@ import Overview from "./components/Overview";
 import Transactions from "./components/Transactions";
 import NavBar from "./components/NavBar";
 import Greetings from "./components/Greetings";
+import Footer from "./components/Footer"; 
 import Budget from "./pages/Budget";
 import { BudgetProvider } from "./pages/BudgetContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="grid grid-cols-4 h-dvh bg-blue-200">
+      <div className="grid grid-cols-4 bg-blue-200 min-h-screen">
         <NavBar />
+        <div className="col-span-3 flex flex-col justify-between ">
+          <main className="col-span-3">
+            <Greetings />
+            <Routes>
+              {/* Home route */}
+              <Route
+                path="/"
+                element={
+                  <>
+                    <section className="grid grid-cols-2 gap-2 mr-2">
+                      <Overview
+                        heading1="Balance"
+                        heading2="Income"
+                        heading3="Expenses"
+                        amount1="R 2 000.00"
+                        amount2="R 15 000.00"
+                        amount3="R 7 000.00"
+                      />
+                      <Analytics />
+                      <Transactions />
+                      <Exchange />
+                    </section>
+                  </>
+                }
+              />
 
-        <main className="col-span-3">
-          <Routes>
-            {/* Home route */}
-            <Route
-              path="/"
-              element={
-                <>
-                  <header>
-                    <Greetings />
-                  </header>
-                  <section className="grid grid-cols-2 gap-2 mr-2">
-                    <Overview
-                      heading1="Balance"
-                      heading2="Income"
-                      heading3="Expenses"
-                      amount1="R 2 000.00"
-                      amount2="R 15 000.00"
-                      amount3="R 7 000.00"
-                    />
-                    <Analytics />
-                    <Transactions />
-                    <Exchange />
-                  </section>
-                </>
-              }
-            />
-
-            {/* Budget route */}
-            <Route
-              path="/budget"
-              element={
-                <BudgetProvider>
-                  <Budget />
-                </BudgetProvider>
-              }
-            />
-          </Routes>
-        </main>
+              {/* Budget route */}
+              <Route
+                path="/budget"
+                element={
+                  <BudgetProvider>
+                    <Budget />
+                  </BudgetProvider>
+                }
+              />
+            </Routes>
+          </main>
+          <footer className="bottom-0 p-4">
+            <Footer className="mt-2 p-4 text-center" />
+          </footer>
+        </div>
       </div>
     </BrowserRouter>
   );
